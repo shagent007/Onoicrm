432f26f6b727a6d1e9b680a0fea4890aca2819d2
shagent007 <shagent118@gmail.com>
2024-12-07 01:50:03 +0600

    asd

---
+++ Onoicrm.Api/Controllers/Admin/ServiceGroupController.cs
@@ -0,0 +1,23 @@
+using Onoicrm.Domain;
+using Onoicrm.Domain.Entities;
+using Onoicrm.DataContext;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Onoicrm.Api.Attributes;
+using Onoicrm.Api.Controllers.Base.Admin;
+using Onoicrm.Api.Controllers.Base.Public;
+
+namespace Onoicrm.Api.Controllers.Public;
+
+[Route("api/v1/public/[controller]")]
+[ApiController]
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = $"{UserRoles.SystemAdministrator}, {UserRoles.Director}, {UserRoles.SiteAdministrator}, {UserRoles.Administrator}, {UserRoles.Dentist}")]
+[ValidateSecurityStamp]
+public class ServiceGroupController: PublicObjectController<ServiceGroup>
+{
+    public ServiceGroupController(IConfiguration configuration, ApplicationDataContext dataContext, UserManager<IdentityUser> userManager) : base(configuration, dataContext,userManager)
+    {
+    }
+}
